1013704a0801908fdd95e035fe9c67fbc8e384d6
shaanu1998 <shantanusinghbisht86@gmail.com>
2021-11-08 10:18:22 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -4,41 +4,58 @@ import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import { useState } from "react";
 import Alert from "./components/Alert";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light')
-  const [alert , setAlert] = useState(null)
-  const showAlert=(message,type)=>{
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type : type}
-      );
+      type: type,
+    });
 
-      setTimeout(() => {
-        setAlert(null);
-      }, 2000);
-     
-  }
-  let toggleMode=()=>{
-    if (mode==='light'){
-      setMode('dark')
-      document.body.style.backgroundColor='#103368';
-      showAlert("Dark mode has been anabled","success");
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+  let toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#103368";
+      showAlert("Dark mode has been anabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been anabled", "success");
     }
-    else {
-      setMode('light')
-      document.body.style.backgroundColor='white';
-      showAlert("Light mode has been anabled","success");
-    }
-  }
+  };
   return (
     <>
-      <Navbar title="shantanu"  about="Aboutus" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert}/>
-      <div className="container my-3">
-      <TextForm  heading="Enter the text here to analyse" mode={mode} showAlert={showAlert} />
-      </div>
-      {/* <About /> */}
+      {/* <Router> */}
+        <Navbar
+          title="shantanu"
+          about="Aboutus"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          {/* <Switch>
+            <Route exact path="/about">
+              <About />
+            </Route>
+
+            <Route exact path="/"> */}
+              <TextForm
+                heading="Enter the text here to analyse"
+                mode={mode}
+                showAlert={showAlert}
+              />
+            {/* </Route>
+          </Switch> */}
+        </div>
+      {/* </Router> */}
     </>
   );
 }
